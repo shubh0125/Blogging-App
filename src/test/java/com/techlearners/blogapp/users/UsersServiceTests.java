48d1348e0a99bc797db7228318507e63e836788a
Shubh Almal <shubhalmal@gmail.com>
2024-02-20 16:29:16 -0500

    Added a file to test whether we can Create a new user or not
    File names "UserServiceTest.java" was added to check whether we can create and add new users or not

---
+++ src/test/java/com/techlearners/blogapp/users/UsersServiceTests.java
@@ -0,0 +1,31 @@
+package com.techlearners.blogapp.users;
+
+
+import com.techlearners.blogapp.users.dtos.CreateUserRequest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class UsersServiceTests {
+
+    @Autowired UserService userService;
+
+    @Test
+    void can_create_users(){
+
+        var user = userService.createUser(new CreateUserRequest(
+                "john",
+                "pass123",
+                "john@gmai.com"
+        ));
+
+        Assertions.assertNotNull(user);
+        Assertions.assertEquals("john", user.getUsername());
+
+    }
+
+}
